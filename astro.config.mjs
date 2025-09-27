7c97de9fa8f377f4d493411192da32536f061b7e
Mattisbbn <mattisbbn@gmail.com>
2025-09-27 23:10:46 +0200

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import astroIcon from 'astro-icon';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    astroIcon({
+      iconify: {
+        collections: ['fa', 'fa-brands', 'fa-solid']
+      }
+    })
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
